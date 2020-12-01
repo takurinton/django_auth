6a18c735c2fc04d5bc044df0ba6cf7de028c57eb
takurinton <takuirnton@gmail.com>
2020-12-01 20:52:45 +0900

    first

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class IndexView(APIView):
+    def get(self, request):
+        me = {
+            'name': 'takurinton', 
+            'age': 21, 
+            'hoge': 'fuga', 
+        }
+        return Response(me)
